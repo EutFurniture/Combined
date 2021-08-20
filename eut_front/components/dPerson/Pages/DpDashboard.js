1a83926b900df6c7d30f59b70edac4cba463de16
subhasthikaG <56733508+subhasthikaG@users.noreply.github.com>
2021-08-20 12:59:15 +0530

    Updated  DP Dashboard

--- eut_front/components/dPerson/Pages/DpDashboard.js
+++ eut_front/components/dPerson/Pages/DpDashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
+import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
@@ -15,7 +16,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import  './DpDashboard.css';
 import {Redirect} from "react-router-dom";
-import { useState } from 'react';
+import { useState  , useEffect} from 'react';
 import { Link } from "react-router-dom"; 
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -23,6 +24,9 @@ import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import { DpListItems, Logout } from './dplistItems';
 import Testimonial  from './Testimonial';
+import {toast} from 'react-toastify'
+
+
 
 function Copyright() {
   return (
@@ -150,9 +154,26 @@ const styles = {
 
 
 
-export default function Dashboard() {
+toast.configure()
+const DpDashboard =()=> {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
+
+  const [returns_count,setReturnsCount]=useState([])
+  useEffect(()=>{
+    axios.get("http://localhost:3001/CountReturnItems").then((response)=>{
+      setReturnsCount(response.data)
+    })
+  },[])
+
+  const [totalcashon_income,setTotalcashonIncome]=useState([])
+  useEffect(()=>{
+    axios.get("http://localhost:3001/TotalcashonIncome").then((response)=>{
+      setTotalcashonIncome(response.data)
+    })
+  },[])
+  
+  
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -264,7 +285,11 @@ if(!isAuth){
                        <div className="charts_right_cards">
                            <div className="card1">
                                <h3>Total Cash On Payments</h3>
-                               <p>Rs.125,300</p>
+                               {totalcashon_income.map((record)=>{
+                                 return(
+                                  <p style={{fontSize:'25px'}}>Rs.{record.income}</p>
+                                 )
+                               })}
                            </div>
 
                            <div className="card2">
@@ -278,8 +303,12 @@ if(!isAuth){
                            </div>
 
                            <div className="card4">
-                               <h3>Total Return Items </h3>
-                               <p>13</p>
+                           <h2>Returned Items</h2>
+                           {returns_count.map((record)=>{
+                                 return(
+                                  <p style={{fontSize:'30px'}}>{record.returncount}</p>
+                                 )
+                               })}
                            </div>
                            <div className="card1">
                                <h3>Free Deliveries </h3>
@@ -367,3 +396,4 @@ if(!isAuth){
     </div>
   );
 }
+export default DpDashboard;
