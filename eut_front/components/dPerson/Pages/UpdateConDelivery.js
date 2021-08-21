94dde2f403777a32e981119dab54194e0d312780
subhasthikaG <56733508+subhasthikaG@users.noreply.github.com>
2021-08-21 12:26:58 +0530

    Updated Confirm Delivery

--- eut_front/components/dPerson/Pages/UpdateConDelivery.js
+++ eut_front/components/dPerson/Pages/UpdateConDelivery.js
@@ -1,16 +1,13 @@
-import React, { useState,useEffect } from "react";
-import clsx from 'clsx';
-import axios from "axios";
 
-import Form from 'react-bootstrap/Form';
-import { Button } from 'react-bootstrap';
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
-import {Redirect, useParams} from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import clsx from 'clsx';
+import React, { useState, useEffect } from "react";
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
+import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
@@ -19,27 +16,44 @@ import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
-import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
-import { useForm } from "react-hook-form";
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
- import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-
-
+import {Redirect} from "react-router-dom"
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import { DpListItems, Logout } from './dplistItems';
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Eut Furniture
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 
-const drawerWidth = 240;
+const drawerWproduct_idth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-   
   },
   toolbar: {
     paddingRight: 24, // keep right padding when drawer closed
@@ -53,15 +67,15 @@ const useStyles = makeStyles((theme) => ({
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(['wproduct_idth', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
   appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    marginLeft: drawerWproduct_idth,
+    wproduct_idth: `calc(100% - ${drawerWproduct_idth}px)`,
+    transition: theme.transitions.create(['wproduct_idth', 'margin'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -69,7 +83,7 @@ const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: 36,
   },
-  menuButtonHidden: {
+  menuButtonHproduct_idden: {
     display: 'none',
   },
   title: {
@@ -77,31 +91,24 @@ const useStyles = makeStyles((theme) => ({
     fontSize:40,
     fontWeight:600,
   },
-  userimage : {
-    height: 60,
-    width: 60,
-    borderRadius:100,
-    borderColor:'white',
-
-  },
   drawerPaper: {
     position: 'relative',
     whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    wproduct_idth: drawerWproduct_idth,
+    transition: theme.transitions.create('wproduct_idth', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
   drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
+    overflowX: 'hproduct_idden',
+    transition: theme.transitions.create('wproduct_idth', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    width: theme.spacing(7),
+    wproduct_idth: theme.spacing(7),
     [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
+      wproduct_idth: theme.spacing(9),
     },
   },
   appBarSpacer: theme.mixins.toolbar,
@@ -109,105 +116,56 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     height: '100vh',
     overflow: 'auto',
-    
+    backgroundColor:'#f0f8ff'
   },
- 
   container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
-    marginTop:'20px',
-    marginLeft:'40px',
+    alignContent:'center',
+    align:'center',
+    
   },
   paper: {
+    position:'relative',
+    align:'center',
     padding: theme.spacing(2),
     display: 'flex',
     overflow: 'auto',
     flexDirection: 'column',
+   
   },
   fixedHeight: {
-    height: 'auto',
+    height: 240,
   },
-  addbutton:{
-      backgroundColor:'#0000ff',
-      height:'50px',
-      width:'160px',
-      borderRadius:'5px',
-      marginRight:'10px',
-      textDecoration:'none',
-      textAlign:'center',
-      paddingTop:'10px'
-  },
-  addcategorybox:{
-    width: '1100px',
-    height:'120px',
-    backgroundColor: '#fff',
-    marginLeft: '30px',
-    display:'flex',
-    //boxShadow:'5px 1px 2px 2px '
-    
-  },
-  categorybtn:{
-      border:0,
-      backgroundColor:'#9bddff',
-      width:'800px',
-      height:'40px',
-      marginTop:'40px',
-      marginLeft:'30px',
-      fontSize:'20px',
-      borderRadius:'5px'
-
-  },
-  addcategory:{
-    height:'40px'
-  },
-  categoryimage:{
-    height:'500px',
-    width:'1100px'
-},
-btn:{
-    color:'white',
-    fontSize:'18px',
-    width:'150px',
-    height:'40px',
-    backgroundColor:'blue',
-    border:'none',
-    borderRadius:'5px'
-},
-addproducts:{
-    display:'flex',
-},
-
   
-
+  imageInput:{
+    border:'none',
+    borderColor:'white'
+  }
 }));
 
 const styles = {
-  side:{
-    backgroundColor:'rgb(37, 37, 94)',
-  }
-};
-
-const dateOnly = (d) => {
-    const date = new Date(d);
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    return `${year} - ${month} - ${day}`;
-  };
-
-
-
-export default function UpdateReturnDetail() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
+  sproduct_ide:{
+    backgroundColor:'rgb(37,37,94)',
+  },
  
   
- const {order_id} = useParams();
- const [Dt, setDt] = useState([])
- const [newO_status,setNewO_status]=useState(0);
- 
+};
 
+
+
+export default function UpdateConDelivery() {
+
+
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const {order_id} = useParams();
+  const [state,setState]=useState({file:'',Bill_image:'',message:'',success:false})
+   const[newActive,setNewActive]=useState();
+  const[newBill_image,setNewBill_image]=useState();
+  const [Dt, setDt] = useState([])
   const [ReturnList, setReturnList] = useState([]);
+
 
 
   useEffect(() => {
@@ -220,22 +178,52 @@ export default function UpdateReturnDetail() {
         });
   
         setDt(response.data[0]);
-           console.log(response.data[0]);
+        setNewActive(response.data[0].active)
+        setNewBill_image (response.data[0].Bill_image)
+        console.log(response.data[0]);
     };
     fetchData();
   }, [order_id]);
 
+    
   const UpdateConDelivery = (order_id) => {
-    axios.put(`http://localhost:3001/confirmdelivery/${order_id}`, {o_status:newO_status ,order_id:order_id}).then(
-      (response) => {
-        setReturnList(Dt.map((record) => {
-          return record.id === order_id ? {order_id: record.order_id, o_status: record.o_status,  o_status:newO_status} : record
-          
-        }))
-     }
-    )
-    alert("Delivery is Confirmed")  
-  };
+      if(state.file)
+      {
+        let formData=new FormData();
+        formData.append('file',state.file) 
+        axios.post('http://localhost:3001/imageUpload',formData,{
+            'content-Type':'multipart/form-data',
+          })
+    
+          axios.put(`http://localhost:3001/confirmdelivery/${order_id}`, { active:newActive, Bill_image:state.file.name, order_id:order_id}).then(
+            (response) => {
+              setReturnList(Dt.map((record) => {
+                return record.id === order_id ? {order_id: record.order_id, active:record.active, Bill_image:record.Bill_image, active:newActive, Bill_image:newBill_image} : record
+                
+              }))
+           }
+          )
+          alert("Delivery is Confirmed")  
+        }
+      
+   
+      
+    };
+
+    const handleInput =(e) =>{
+      let reader =new FileReader();
+      let file=e.target.files[0]
+      reader.onloadend =() =>{
+        setState({
+          ...state,
+          file:file,
+          Bill_image:reader.result,
+          message:""
+        })
+       
+      }
+      reader.readAsDataURL(file);
+    }
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -243,6 +231,7 @@ export default function UpdateReturnDetail() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleClick = (event) => {
@@ -253,18 +242,19 @@ export default function UpdateReturnDetail() {
     setAnchorEl(null);
   };
 
-
-  const[isAuth,setIsAuth]=useState(true);
+   
+    const[isAuth,setIsAuth]=useState(true);
 
   if(!isAuth){
     return <Redirect to="" />
   }
 
+
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar} style={{backgroundColor: 'rgb(37, 37, 94)'}}>
+        <Toolbar className={classes.toolbar} style={{backgroundColor: 'rgb(37,37,94)'}}>
           <IconButton
             edge="start"
             color="inherit"
@@ -275,29 +265,26 @@ export default function UpdateReturnDetail() {
             <MenuIcon />
           </IconButton>
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            <b>DELIVERY PERSON</b>
+            <strong>ADMIN</strong>
           </Typography>
 
-
-         
           <IconButton color="inherit" fontSize="inherit">
            <AccountCircleIcon   onClick={handleClick}/>
   
           </IconButton>
           <Menu
-        id="simple-menu"
+        product_id="simple-menu"
         anchorEl={anchorEl}
         keepMounted
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
         <MenuItem onClick={()=>setIsAuth(false)}>Logout</MenuItem>
       </Menu>
-
         </Toolbar>
-        
       </AppBar>
-      <div style={styles.side}>
+      <div style={styles.sproduct_ide}>
       <Drawer
         variant="permanent"
         classes={{
@@ -305,79 +292,97 @@ export default function UpdateReturnDetail() {
         }}
         open={open}
       >
-        <div className={classes.toolbarIcon} style={{backgroundColor: 'rgb(37, 37, 94)', color:'white'}}>
+        <div className={classes.toolbarIcon} style={{backgroundColor: 'rgb(37,37,94)', color:'white'}}>
           <IconButton onClick={handleDrawerClose} style={{color:'white'}}>
             <ChevronLeftIcon />
           </IconButton>
         </div>
-        <Divider/>
-        <List style={{backgroundColor: 'rgb(37, 37, 94)', color:'white'}}>{DpListItems}</List>
-        <Divider/>
-        <List style={{backgroundColor: 'rgb(37, 37, 94)', color:'red'}} onClick={()=>setIsAuth(false)}>{Logout}</List>
-        <Divider/>
+        <Divider />
+        <List style={{backgroundColor: 'rgb(37,37,94)', color:'white'}}>{DpListItems}</List>
+        
+        <Divider />
+        <List style={{backgroundColor: 'rgb(37,37,94)', color:'red'}}>{Logout}</List>
+        <Divider />
       </Drawer>
       </div>
-     
-      <main style={{backgroundColor: '#f0f8ff'}} className={classes.content}>
+      <main className={classes.content}>
         <div className={classes.appBarSpacer} />
-        <Container  maxWidth="lg" className={classes.container}>
+        <Container maxWproduct_idth="lg" className={classes.container}>
+          <Grid container spacing={18}>
         
-        <Grid  container spacing={10}>
-      
-        <Grid item xs={11}  direction="row"  >
-      
-        <div >
-           <Paper className={classes.paper}>
-               
-           <Typography component="h1" variant="h6" color="inherit" align="center" width="100%" noWrap className={classes.title}>
-                  <strong> CONFIRM DELIVERY </strong>
-                </Typography><br/>
+            
+            
 
-                <Form >
-                <Form.Group as={Row} controlId="formHorizontalName">
-                  <Form.Label column lg={2} >
-                   Order Id :
-                  </Form.Label>
-                  <Col >
-                  <Form.Label column lg={2} >
-                   {Dt.order_id}
-                  </Form.Label>
-                  </Col>
-              </Form.Group><br/>
-
-
-              <Form.Group as={Row} controlId="formHorizontalName">
-                  <Form.Label column lg={2} >
-                   Delivery Status :
-                  </Form.Label>
-                  <Col >
-                  <Form.Control as="select" defaultValue={Dt.o_status}  onChange={(event)=> { setNewO_status(event.target.value);}} >
-                 
-                      <option value="Completed">Completed</option>
-                      <option value="Ready to deliver">Ready to deliver</option>
-                      <option value="Processing">Processing</option>
-                      <option value="Pending">Pending</option>
-                      <option value="Returned">Returned</option>
-                      </Form.Control>                    
-                  </Col>
-                </Form.Group><br/>
-
- 
-              
-                <div align="center">
-                 <Button  style={{fontSize:'20px',width:'200px'}} type="submit" onClick={() => {UpdateConDelivery(Dt.order_id)}} >Update</Button>
-                 </div> 
-               
-         </Form>
-                
+            {/* Recent Orders */}
+            <Grid item xs={11} direction="row"  >
+            
   
-    
-          </Paper>
-         </div>
-        
-        </Grid>
+            <div >
+              <Paper className={classes.paper}>
+              <Typography component="h1" variant="h6" color="inherit"  align="center" width="100%" noWrap className={classes.title}>
+              <strong>UPDATE PRODUCT DETAILS</strong>
+            </Typography><br/>
+            <Form >
 
-      </Grid>
+   <Form.Group as={Row} controlId="formHorizontalName">
+     <Form.Label column lg={2} >
+     Order Id :
+     </Form.Label>
+     <Col >
+     <Form.Label column lg={2} >
+        {Dt.order_id}
+     </Form.Label>
+      </Col>
+   </Form.Group><br/>
+  
+   <Form.Group as={Row} controlproduct_id="formHorizontalQuantity">
+     <Form.Label column lg={2} >
+     Active :
+     </Form.Label>
+     <Col >
+       <Form.Control type="text" defaultValue={newActive}
+       onChange={(event)=> {
+         setNewActive(event.target.value);
+       }}
+       />
+     </Col>
+   </Form.Group><br/>
+  
+   
+
+   <Form.Group as={Row} controlId="formHorizontalFile" className="mb-3">
+     <Form.Label column lg={2}>
+      Bill Image :</Form.Label>
+     <Col >
+     <Form.Control type="file"  defaultValue={newBill_image} className={classes.imageInput}
+      onChange={handleInput}
+     />                  
+     </Col>
+     </Form.Group>  
+    
+     {state.message && <h6 className={classes.mess}>{state.message}</h6>}            
+     <div style={{marginLeft:'227px'}}>
+   {state.Bill_image && (<img src={state.Bill_image}  width="20%" height="20%"  alt="preview" />)}
+   </div><br/>
+
+   
+       <div align="center">
+       <Button  style={{fontSize:'20px',width:'200px'}} type="submit" onClick={() => {UpdateConDelivery(Dt.order_id)}} >Update</Button>
+       </div>
+      
+
+</Form>
+                
+            
+              </Paper>
+              </div>
+            </Grid>
+ 
+          </Grid>
+          
+          <Box pt={4}>
+            <Copyright />
+          </Box>
         </Container>
       </main>
     </div>
