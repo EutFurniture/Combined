cd171fe570670ce4a54684e54c6d5d1d85234f3f
Daranya <daranyasehar@gmail.com>
2021-09-04 19:28:39 +0530

    Update OrdersUI.js
    Orders

--- eut_front/components/sManager/pages/OrdersUI.js
+++ eut_front/components/sManager/pages/OrdersUI.js
@@ -40,12 +40,13 @@ class OrdersUI extends Component{
         <Table striped bordered hover>
         <thead>
           <tr>
-          <td align="center"><h6>#</h6></td>
-          <td align="center"><h6>Product</h6></td>
-          <td align="center"><h6>Price</h6></td>
-          <td></td>
-          <td></td>
-          <td></td>
+         
+          <td align="center"><h6>Status</h6></td>
+          <td align="center"><h6>Cust ID</h6></td>
+          <td align="center"><h6>Date</h6></td>
+          <td align="center"><h6>DueDate</h6></td>
+          <td align="center"><h6>Quantity</h6></td>
+          <td align="center"><h6>Amount</h6></td>
          
           </tr>
           </thead>
@@ -53,12 +54,12 @@ class OrdersUI extends Component{
           {items.map(item => (
            
             <tr key={item.order_id}>
-              <td align="center">{item.order_id}</td>
-              <td align="center">{item.order_name}</td>
-              <td align="center">{item.payment}</td>
-              <td align="center"><StatusOrderPU/></td>
-              <td align="center"><ViewOrderPU/></td>
-              <td align="center"><DetailOrderPU/></td>
+              <td align="center">{item.status}</td>
+              <td align="center">{item.customer_id}</td>
+              <td align="center">{item.o_date}</td>
+              <td align="center">{item.order_last_date}</td>
+              <td align="center">{item.quantity}</td>
+              <td align="center">{item.total}</td>
             </tr>
                  
           ))}
