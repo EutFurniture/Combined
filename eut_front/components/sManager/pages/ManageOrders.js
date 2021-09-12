a66c2f0b48c1c7fce2b4537f6bb178224ac71c53
Daranya <daranyasehar@gmail.com>
2021-09-12 18:22:54 +0530

    ManageOrders

--- eut_front/components/sManager/pages/ManageOrders.js
+++ eut_front/components/sManager/pages/ManageOrders.js
@@ -1,3 +1,248 @@
+// import { Link } from "react-router-dom";
+// import clsx from 'clsx';
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import CustomView from './CustomView';
+// import {Redirect} from "react-router-dom"
+// import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+// import "../../../css/manageCustom.css"
+
+// import { makeStyles } from '@material-ui/core/styles';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Drawer from '@material-ui/core/Drawer';
+// import Box from '@material-ui/core/Box';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import List from '@material-ui/core/List';
+// import Typography from '@material-ui/core/Typography';
+// import Divider from '@material-ui/core/Divider';
+// import IconButton from '@material-ui/core/IconButton';
+// import Badge from '@material-ui/core/Badge';
+// import Container from '@material-ui/core/Container';
+// import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+// import NotificationsIcon from '@material-ui/icons/Notifications';
+// import Menu from '@material-ui/core/Menu';
+// import MenuItem from '@material-ui/core/MenuItem';
+
+// import OrdersView from './OrdersView';
+
+// import { mainListItems, Logout } from './listItems';
+
+// const drawerWidth = 240;
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//   },
+//   toolbar: {
+//     paddingRight: 24, // keep right padding when drawer closed
+//   },
+//   toolbarIcon: {
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'flex-end',
+//     padding: '0 8px',
+//     ...theme.mixins.toolbar,
+//   },
+//   appBar: {
+//     zIndex: theme.zIndex.drawer + 1,
+//     transition: theme.transitions.create(['width', 'margin'], {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//   },
+//   appBarShift: {
+//     marginLeft: drawerWidth,
+//     width: `calc(100% - ${drawerWidth}px)`,
+//     transition: theme.transitions.create(['width', 'margin'], {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//   },
+//   menuButton: {
+//     marginRight: 36,
+//   },
+//   menuButtonHidden: {
+//     display: 'none',
+//   },
+//   title: {
+//     flexGrow: 1,
+//     fontSize:40,
+//     fontWeight:600,
+//   },
+//   userimage : {
+//     height: 60,
+//     width: 60,
+//     borderRadius:100,
+//     borderColor:'white',
+  
+//   },
+//   drawerPaper: {
+//     position: 'relative',
+//     whiteSpace: 'nowrap',
+//     width: drawerWidth,
+//     transition: theme.transitions.create('width', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//   },
+//   drawerPaperClose: {
+//     overflowX: 'hidden',
+//     transition: theme.transitions.create('width', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//     width: theme.spacing(7),
+//     [theme.breakpoints.up('sm')]: {
+//       width: theme.spacing(9),
+//     },
+//   },
+//   appBarSpacer: theme.mixins.toolbar,
+//   content: {
+//     flexGrow: 1,
+//     height: '100vh',
+//     overflow: 'auto',
+//   },
+//   container: {
+//     paddingTop: theme.spacing(4),
+//     paddingBottom: theme.spacing(4),
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     display: 'flex',
+//     overflow: 'auto',
+//     flexDirection: 'column',
+//   },
+//   fixedHeight: {
+//     height: 240,
+//   },
+  
+// }));
+
+// const styles = {
+//   side:{
+//     backgroundColor:'rgb(37, 37, 94)',
+//   }
+// };
+
+
+// export default function ManageOrders() {
+//   const classes = useStyles();
+//   const [open, setOpen] = React.useState(true);
+//   const handleDrawerOpen = () => {
+//     setOpen(true);
+//   };
+//   const handleDrawerClose = () => {
+//     setOpen(false);
+//   };
+//   const [anchorEl, setAnchorEl] = React.useState(null);
+
+//   const handleClick = (event) => {
+//     setAnchorEl(event.currentTarget);
+//   };
+
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
+//   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+//   const[isAuth,setIsAuth]=useState(true);
+
+//   if(!isAuth){
+//     return <Redirect to="" />
+//   }
+
+//   return (
+//     <div className={classes.root}>
+//       <CssBaseline />
+//       <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+//         <Toolbar className={classes.toolbar} style={{backgroundColor: 'rgb(37, 37, 94)'}}>
+//           <IconButton
+//             edge="start"
+//             color="inherit"
+//             aria-label="open drawer"
+//             onClick={handleDrawerOpen}
+//             className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+//             <b>Sales Manager</b>
+//           </Typography>
+//           <IconButton color="inherit">
+//             <Badge badgeContent={4} color="secondary">
+//               <NotificationsIcon />
+//             </Badge>
+//           </IconButton>
+//           <IconButton color="inherit" fontSize="inherit">
+//            <AccountCircleIcon   onClick={handleClick}/>
+  
+//           </IconButton>
+//    <Menu
+//         id="simple-menu"
+//         anchorEl={anchorEl}
+//         keepMounted
+//         open={Boolean(anchorEl)}
+//         onClose={handleClose}
+//       >
+//         <MenuItem onClick={handleClose}>Profile</MenuItem>
+//         <MenuItem onClick={()=>setIsAuth(false)}>Logout</MenuItem>
+//       </Menu>
+//         </Toolbar>
+//       </AppBar>
+//       <div style={styles.side}>
+//       <Drawer
+//         variant="permanent"
+//         classes={{
+//           paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+//         }}
+//         open={open}
+//       >
+//         <div className={classes.toolbarIcon} style={{backgroundColor: 'rgb(37, 37, 94)', color:'white'}}>
+//           <IconButton onClick={handleDrawerClose} style={{color:'white'}}>
+//             <ChevronLeftIcon />
+//           </IconButton>
+//         </div>
+//         <Divider/> 
+//         <List style={{backgroundColor: 'rgb(37, 37, 94)', color:'white'}}>{mainListItems}</List>
+//         <Divider/>
+//         <List style={{backgroundColor: 'rgb(37, 37, 94)', color:'red'}} onClick={()=>setIsAuth(false)}>{Logout}</List>
+//        <Divider/>
+//       </Drawer>
+//       </div>
+      
+//       <main style={{backgroundColor: '#f0f8ff'}} className={classes.content}>
+//         <div className={classes.appBarSpacer} />
+//         <Container maxWidth="lg" className={classes.container}>
+        
+//         <Grid container spacing={3}>
+//         {/* Recent Orders */}
+//         <Grid item xs={12}  direction="row"  >
+        
+//         <div >
+        
+//            <Paper className={classes.paper}>
+               
+//                 <Typography  component="h1" variant="h6" color="inherit" align="center" width="100%" noWrap className={classes.title}>
+//                   <h1>Details of Orders</h1>
+//                 </Typography>
+//                 <OrdersView/>
+                     
+//           </Paper>
+//           </div>
+//         </Grid>
+
+//       </Grid>
+//         </Container>
+//       </main>
+//     </div>
+//   );
+// }
+
 import { Link } from "react-router-dom";
 import clsx from 'clsx';
 import React, { useState, useEffect } from "react";
@@ -5,7 +250,9 @@ import axios from "axios";
 import CustomView from './CustomView';
 import {Redirect} from "react-router-dom"
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-
+import Axios from "axios";
+import {toast} from 'react-toastify'
+import { Button } from 'react-bootstrap';
 import "../../../css/manageCustom.css"
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -140,6 +387,64 @@ export default function ManageOrders() {
     setOpen(false);
   };
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [orderNotifyCount,setorderNotifyCount]=useState([]);
+
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/ordernotifyCount").then((response)=>{
+      setorderNotifyCount(response.data)
+      
+    })
+  },[])
+  
+  const ordercount=orderNotifyCount.map(record=>record.o_count);
+  console.log(ordercount);
+  
+  
+  
+  
+  const [orderNotifymess,setorderNotifymess]=useState([])
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/ordernotifymess").then((response)=>{
+      setorderNotifymess(response.data)
+      
+    })
+  },[])
+  const ordermesscount=orderNotifymess.map(record=>record.o_count);
+  
+  
+  const total = Number(ordercount)
+  
+  const NotificationClick = async () => {
+   
+  
+    const responsee = await Axios.get('http://localhost:3001/ordernotifyDeactive', {
+    });
+  
+  
+      if(ordermesscount>0)
+      {
+        const customToastse=()=>{
+          return(
+            <div style={{fontSize:'15px'}}>
+              You have New {ordermesscount} Orders! <br></br><br></br>
+              <Button variant="contained"  onClick={Notification_page_order}>View</Button>
+            </div>
+          )
+        }
+  
+        const notifyee=()=>{
+       
+          toast.info(customToastse,{position:toast.POSITION.TOP_RIGHT,autoClose:false})
+        
+        
+            }
+        notifyee();
+      }
+  
+        const Notification_page_order=()=>{
+          window.location.href='/sManager/pages/Notification_order'
+          }
+  }
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -174,8 +479,8 @@ export default function ManageOrders() {
             <b>Sales Manager</b>
           </Typography>
           <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
+            <Badge badgeContent={total} color="secondary">
+              <NotificationsIcon onClick={NotificationClick}/>
             </Badge>
           </IconButton>
           <IconButton color="inherit" fontSize="inherit">
@@ -242,5 +547,6 @@ export default function ManageOrders() {
     </div>
   );
 }
+
 
 
